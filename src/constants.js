4276768a12c5259fce93d05e5802273b45071bf2
becky <becky.naylor@simomics.com>
2019-01-26 17:11:55 +0000

    Updated image size in constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 
 export default {
-    game_width: 5181,
-    game_height: 799,
+    game_width: 8420,
+    game_height: 800,
     viewport_width: 1200,
-    viewport_height: 799,
+    viewport_height: 800,
 }
