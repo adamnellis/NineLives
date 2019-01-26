b6d5d52a7ab54e37ddc41b31405965efd3593aac
Adam Nellis <adam.nellis@simomics.com>
2019-01-26 10:50:08 +0000

    Added particles

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('red-particle', 'assets/particles/red.png');
+    this.load.image('flame-particle', 'assets/particles/flame1.png');
 }
 
 function create ()
@@ -29,6 +31,27 @@ function create ()
         ease: 'Power2',
         yoyo: true,
         loop: -1
+    });
+
+    var particles = this.add.particles('red-particle');
+
+    var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    emitter.startFollow(logo);
+
+    var particles_fire = this.add.particles('flame-particle');
+    var emitter_fire = particles_fire.createEmitter({
+        speed: 100,
+        frequency: 1,
+        maxVelocityX: 10,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD',
+        x: {min: 700, max: 750},
+        y: 500,
     });
 
 }
